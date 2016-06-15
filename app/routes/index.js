da315978d018fb41c8514314ef0a9d88f0a803a6
slfweb <slf.pro@gmail.com>
2016-06-15 23:21:02 +0000

    final version

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,31 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var parser = require('ua-parser-js');
 
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
+module.exports = function (app) {
+	
+	var getClientOS = function (req) {
+		var uastring = req.headers['user-agent'];
+    	var ua = parser(uastring);
+    	console.log(ua);
+    	return ua.os.name + ' ' + ua.os.version;
 	}
+	
+	var getClientAddress = function (req) {
+        return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+        || req.connection.remoteAddress;
+	};
 
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
+	app.route('/api/whoami')
 		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+			var ip = req.headers['x-forwarded-for'];
+			var language = req.headers["accept-language"].split(',')[0];
+			var software = getClientOS(req);
+			res.json({
+				ip: ip,
+				language: language,
+				software: software
+			});
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
